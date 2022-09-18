8a3a0f52967b059dfbe2c267ecf8ba1921fb8b25
maulik_parmar_mentora <maulik@mentora.institute>
2022-09-18 22:25:03 +0530

    Added MSW Package & Setup server & Pull Check

---
+++ src/pages/Entry/tests/Options.test.jsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+
+import Options from "../Options";
+
+test("displays image for each scoop option from server", async () => {
+  render(<Options optionType="scoops" />);
+
+  // find images
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
+
+  // confirm alt text of images
+  // @ts-ignore
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("displays image for each toppings option from server", async () => {
+  // Mock Services worker will return three toppings from the server
+  render(<Options optionType="toppings" />);
+
+  // find images, expect 3 based on what msw returns
+  const images = await screen.findAllByRole("img", { name: /toppings$/i });
+  expect(images).toHaveLength(3);
+
+  // confirm alt text of images
+  // @ts-ignore
+  const imageTitles = images.map((img) => img.alt);
+  expect(imageTitles).toEqual([
+    "Cherries toppings",
+    "M&Ms toppings",
+    "Hot fudge toppings",
+  ]);
+});
